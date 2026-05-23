972dcd4b2b36ec8b7ea9e3ec24524c84f9256780
Conroy Makhubu <cmakhubu@gmail.com>
2026-05-23 17:54:47 +0200

    Update dart_task3.dart
    create a class MenuItem with private properties

---
+++ dart_task3.dart
@@ -0,0 +1,41 @@
+//superclass MenuItem
+import 'dart:html_common';
+
+class MenuItem {
+  String _name;
+  double _price;
+  String _category;
+//constructor
+  MenuItem(this._name, this._price, this._category);
+  //getters functions
+  String get name {
+    return _name;
+  }
+
+  double get price {
+    return _price;
+  }
+
+  String get category {
+    return _category;
+  }
+
+//setters functions
+  void set name(String name) {
+    _name = name;
+  }
+
+  void set price(double price) {
+    _price = price;
+  }
+
+  void set category(String category) {
+    _category = category;
+  }
+
+  void displayItem() {
+    print('Name: $_name');
+    print('Price: \$$_price');
+    print('Category: $_category');
+  }
+}
