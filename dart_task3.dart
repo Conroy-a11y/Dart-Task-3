e93cc7df139e5e7abbe3f36f40ae6fccb480ae92
Conroy Makhubu <cmakhubu@gmail.com>
2026-05-23 18:08:04 +0200

    Update dart_task3.dart
    change displayItem() to display()

--- dart_task3.dart
+++ dart_task3.dart
@@ -33,7 +33,7 @@ class MenuItem {
     _category = category;
   }
 
-  void displayItem() {
+  void display() {
     print('Name: $_name');
     print('Price: \$$_price');
     print('Category: $_category');
